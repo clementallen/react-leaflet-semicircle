036c71cb75402649542932c4811266590795122e
Clement Allen <clementallen313@gmail.com>
2019-01-13 20:20:46 +0000

    Added more component tests

---
+++ __tests__/SemiCircle.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Map } from 'react-leaflet';
+import L from 'leaflet';
+import { SemiCircle } from '../src/index';
+import AbstractComponent from '../src/AbstractComponent';
+
+jest.spyOn(L, 'SemiCircle').mockImplementation(() => {
+    return {
+        _layerAdd: () => {}
+    };
+});
+
+describe('<SemiCircle />', () => {
+    it('should extend the AbstractComponent', () => {
+        const wrapper = mount(
+            <Map>
+                <SemiCircle />
+            </Map>
+        );
+        expect(
+            wrapper
+                .find(SemiCircle)
+                .children()
+                .instance()
+        ).toBeInstanceOf(AbstractComponent);
+    });
+});
